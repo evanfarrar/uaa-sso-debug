3b13a3449c553c16811dba3cc71880ad8aff34f2
Evan Farrar <efarrar@pivotal.io>
2015-11-24 10:58:08 -0800

    Add missing dependency

---
+++ Godeps/_workspace/src/github.com/evanfarrar/uaa-sso-golang/uaa/get_token_key.go
@@ -0,0 +1,42 @@
+package uaa
+
+import (
+	"encoding/json"
+	"net/url"
+	"fmt"
+)
+
+type GetTokenKeyInterface interface {
+	GetTokenKey() (string, error)
+}
+
+func GetTokenKey(u UAA) (string, error) {
+	tokenURL := u.uaaURL + "/token_key"
+	uri, err := url.Parse(tokenURL)
+	if err != nil {
+		return "", err
+	}
+
+	token, err := GetClientToken(u)
+	fmt.Println("======> Inside of get token key")
+	if err != nil {
+		fmt.Println(err.Error())
+		return "", err
+	}
+
+	host := uri.Scheme + "://" + uri.Host
+
+	client := NewClient(host, u.VerifySSL).WithAuthorizationToken(token.Access)
+	code, body, err := client.MakeRequest("GET", uri.RequestURI(), nil)
+	if err != nil {
+		return "", err
+	}
+
+	if code > 399 {
+		return "", NewFailure(code, body)
+	}
+
+	hash := make(map[string]interface{})
+	json.Unmarshal(body, &hash)
+	return hash["value"].(string), nil
+}
